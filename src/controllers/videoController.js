70d2ee245a40d227c9eb28a6cd9b1570eaa5596a
Suhyun_Lim <suhuyn19@naver.com>
2021-10-05 22:12:14 +0900

    21-10-05

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,25 +1,52 @@
 const trending = (req, res) => {
-  return res.render("home", {pageTitle: 'Home'});
+  const videos = [
+    {
+      title: 'first Video',
+      rating: 5,
+      comments: 2,
+      createdAt: '2 Minutes ago',
+      views: 59,
+      id: 1,
+    },
+    {
+      title: 'first Video',
+      rating: 5,
+      comments: 2,
+      createdAt: '2 Minutes ago',
+      views: 59,
+      id: 1,
+    },
+    {
+      title: 'first Video',
+      rating: 5,
+      comments: 2,
+      createdAt: '2 Minutes ago',
+      views: 59,
+      id: 1,
+    },
+  ];
+
+  return res.render('home', { pageTitle: 'Home', videos });
 };
 
-const see = (req, res) =>  res.render('watch');
+const see = (req, res) => res.render('watch');
 
 const edit = (req, res) => {
-  console.log(req.params)
-  return res.send("Edit");
+  console.log(req.params);
+  return res.send('Edit');
 };
 
 const search = (req, res) => {
-  return res.send("Search")
-}
+  return res.send('Search');
+};
 
 const upload = (req, res) => {
-  return res.send("Upload");
-}
+  return res.send('Upload');
+};
 
 const deleteVideo = (req, res) => {
-  console.log(req.params)
-  return res.send("Delete")
-}
+  console.log(req.params);
+  return res.send('Delete');
+};
 
 export { trending, see, edit, search, upload, deleteVideo };
